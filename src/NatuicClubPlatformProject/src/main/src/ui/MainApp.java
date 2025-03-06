889b77977882e1b15b4e0e254c50d2fbb2714ca8
IbrahimAmine223 <MohamedAmine.ibrahim@esprit.tn>
2025-03-06 23:45:13 +0100

    Remove files from GitHub but keep locally

--- src/NatuicClubPlatformProject/src/main/src/ui/MainApp.java
+++
@@ -1,9 +0,0 @@
-package ui;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import java.io.IOException;
-
-public class MainApp {
-}
